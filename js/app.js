d9e5091581bbdc9bd13e2f5acdca15985732be63
Lee Tien Sinh <muzankibut112@gmail.com>
2022-09-09 15:51:14 +0700

    init

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.controller("myCtrl", function ($scope) { });
+
+app.controller(
+  "contactsController",
+  function ($scope, $routeParams, $location, $http) {
+    $scope.emails = {
+      EMAIL_FORMAT:
+        /^\w+([\.-]?\w+)*@(list.)?gmail.com+((\s*)+,(\s*)+\w+([\.-]?\w+)*@(list.)?gmail.com)*$/,
+      EMAIL_FORMAT_HELP: "Email... Ex: example@example.com",
+    };
+    $scope.submit = function (name, email, subject, mess) {
+      $scope.contacts = JSON.parse(localStorage.getItem("contact") || "[]");
+      $scope.data = {
+        name,
+        email,
+        subject,
+        mess,
+      };
+      if (!name || !email || !subject || !mess) {
+        alert("Name, Email and Subject must be provided");
+      } else {
+        $scope.contacts.push($scope.data);
+        localStorage.setItem("contact", JSON.stringify($scope.contacts));
+        alert("Thanks for contacting");
+        console.log($scope.contacts);
+        $location.path("/");
+      }
+    };
+  }
+);
+
+
+app.config([
+  "$routeProvider",
+  function ($routeProvider) {
+    $routeProvider.when("/", {
+      templateUrl: "pages/home.html",
+    });
+  },
+]);
